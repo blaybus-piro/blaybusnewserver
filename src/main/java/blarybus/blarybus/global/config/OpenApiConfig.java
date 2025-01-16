b9a80d37cfa8b9a480917b185479a9b8080db083
chltmdgh522 <chltmdgh517@naver.com>
2025-01-17 01:04:58 +0900

    feat: jwt 토큰 및 error 커스텀 생성

---
+++ src/main/java/blarybus/blarybus/global/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package blarybus.blarybus.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "??? 명세서",
+                description = "API 명세서",
+                version = "v1",
+                contact = @Contact(
+                        name = "최승호",
+                        email = "chltmdgh517@naver.com"
+                )
+        )
+)
+@Configuration
+public class OpenApiConfig {
+}
