61145cc19d478a65a32ce3ac5d09b95284b8229c
devkev00 <kimminsoo116@gmail.com>
2025-02-15 01:58:00 +0900

    chore(#9) :: 임시방편...

--- src/main/java/blaybus/domain/meeting/presentation/controller/MeetingController.java
+++ src/main/java/blaybus/domain/meeting/presentation/controller/MeetingController.java
@@ -1,36 +1,37 @@
-package blaybus.domain.meeting.presentation.controller;
-
-import blaybus.domain.meeting.application.service.MeetingService;
-import blaybus.domain.meeting.entity.Meeting;
-import blaybus.domain.meeting.presentation.dto.request.MeetingCreateRequest;
-import blaybus.domain.meeting.presentation.dto.response.MeetingResponse;
-import blaybus.domain.meeting.repository.MeetingRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/meetings")
-@RequiredArgsConstructor
-public class MeetingController {
-    private final MeetingService meetingService;
-    private final MeetingRepository meetingRepository;
-
-    @PostMapping
-    public MeetingResponse createMeeting(@RequestBody MeetingCreateRequest request) {
-        // 링크 생성 및 title 받기
-        MeetingResponse meetingResponse = meetingService.createMeeting(request);
-
-        Meeting meeting = Meeting.builder()
-                .title(meetingResponse.title())  // 서비스에서 생성된 title 사용
-                .meetUrl(meetingResponse.hangoutLink())
-                .startTime(request.startTime())
-                .endTime(request.endTime())
-                .build();
-        meetingRepository.save(meeting);
-
-        return meetingResponse;
-    }
-}
+//package blaybus.domain.meeting.presentation.controller;
+//
+//import blaybus.domain.meeting.application.service.MeetingService;
+//import blaybus.domain.meeting.entity.Meeting;
+//import blaybus.domain.meeting.presentation.dto.request.MeetingCreateRequest;
+//import blaybus.domain.meeting.presentation.dto.response.MeetingResponse;
+//import blaybus.domain.meeting.repository.MeetingRepository;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/meetings")
+//@RequiredArgsConstructor
+//public class MeetingController {
+//    private final MeetingService meetingService;
+//    private final MeetingRepository meetingRepository;
+//
+//    @PostMapping
+//    public MeetingResponse createMeeting(@RequestBody @Valid MeetingCreateRequest request) {
+//        // 링크 생성 및 title 받기
+//        MeetingResponse meetingResponse = meetingService.createMeeting(request);
+//
+//        Meeting meeting = Meeting.builder()
+//                .title(meetingResponse.title())  // 서비스에서 생성된 title 사용
+//                .meetUrl(meetingResponse.hangoutLink())
+//                .startTime(request.startTime())
+//                .endTime(request.endTime())
+//                .build();
+//        meetingRepository.save(meeting);
+//
+//        return meetingResponse;
+//    }
+//}
