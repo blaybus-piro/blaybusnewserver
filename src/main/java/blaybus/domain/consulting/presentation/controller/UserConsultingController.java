e8fa5c4bcdbc4a132056e901fbd63efc5dba5af8
touhou09 <touhou09@gmail.com>
2025-02-16 15:04:23 +0900

    fix :: /api add

--- src/main/java/blaybus/domain/consulting/presentation/controller/UserConsultingController.java
+++ src/main/java/blaybus/domain/consulting/presentation/controller/UserConsultingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/consulting")
+@RequestMapping("/api/consulting")
 @RequiredArgsConstructor
 public class UserConsultingController {
 
--- src/main/java/blaybus/domain/designer/presentation/controller/GetOfflineConsultingDesignersController.java
+++ src/main/java/blaybus/domain/designer/presentation/controller/GetOfflineConsultingDesignersController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/designers/offline")
+@RequestMapping("/api/designers/offline")
 @RequiredArgsConstructor
 public class GetOfflineConsultingDesignersController {
 
