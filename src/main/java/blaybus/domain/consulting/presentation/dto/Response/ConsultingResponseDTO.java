f3c91139ca5985b63ba56100644bce29b5e26e67
touhou09 <touhou09@gmail.com>
2025-02-16 09:35:18 +0900

    fix :: meeting - consulting 연결 및 responseDTO 생성 및 수정

---
+++ src/main/java/blaybus/domain/consulting/presentation/dto/Response/ConsultingResponseDTO.java
@@ -0,0 +1,17 @@
+package blaybus.domain.consulting.presentation.dto.Response;
+
+
+import blaybus.domain.consulting.domain.entity.ConsultingType;
+import blaybus.domain.consulting.domain.entity.ConsultingStatus;
+import blaybus.domain.meeting.entity.Meeting;
+import jakarta.validation.constraints.NotNull;
+
+public record ConsultingResponseDTO(
+        @NotNull String userId,
+        @NotNull String designerId,
+        Meeting meeting,
+        @NotNull ConsultingType type,
+        ConsultingStatus status
+)
+{ }
+
