ded7a8763f0718aa1ccfdc97a2132e75e35a8083
lian08 <lian080408@gmail.com>
2025-02-18 00:54:09 +0900

    feat(#2) UserConsultingController Updated

--- src/main/java/blaybus/domain/designer/domain/repository/DesignerRepository.java
+++ src/main/java/blaybus/domain/designer/domain/repository/DesignerRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import blaybus.domain.designer.domain.entity.Type;
-
 import java.util.List;
 
 public interface DesignerRepository extends JpaRepository<Designer, String> {
@@ -29,14 +27,14 @@ public interface DesignerRepository extends JpaRepository<Designer, String> {
     """)
     List<Designer> findOfflineConsultingDesigners(@Param("sortOrder") String sortOrder);
 
-    @Query("""
-    SELECT d FROM Designer d 
-    WHERE d.position.name IN :names 
-    AND (d.type = :type OR d.type = 'BOTH') 
-    ORDER BY FIELD(d.position.name, :names), d.name ASC
-""")
-    List<Designer> findAllByPositionNameAndTypeInOrderByCustomOrder(
-            @Param("names") List<String> names,
-            @Param("type") Type type);
+//    @Query("""
+//    SELECT d FROM Designer d
+//    WHERE d.position.name IN :names
+//    AND (d.type = :type OR d.type = 'BOTH')
+//    ORDER BY FIELD(d.position.name, :names), d.name ASC
+//""")
+//    List<Designer> findAllByPositionNameAndTypeInOrderByCustomOrder(
+//            @Param("names") List<PositionResponseDTO> names,
+//            @Param("type") Type type);
 
 }
