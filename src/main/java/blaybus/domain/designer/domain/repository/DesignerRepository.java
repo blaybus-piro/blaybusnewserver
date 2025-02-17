c866cbe1cad26d4b5235ecad75a8d312ba229ef8
lian08 <lian080408@gmail.com>
2025-02-18 01:43:32 +0900

    feat(#2) DesignerRepository

--- src/main/java/blaybus/domain/designer/domain/repository/DesignerRepository.java
+++ src/main/java/blaybus/domain/designer/domain/repository/DesignerRepository.java
@@ -4,9 +4,11 @@ import blaybus.domain.designer.domain.entity.Designer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DesignerRepository extends JpaRepository<Designer, String> {
 
     @Query("""
