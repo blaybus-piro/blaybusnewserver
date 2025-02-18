7389d085cf27c165c1cdf9daffcd3c6a2f47eeb4
chltmdgh522 <chltmdgh517@naver.com>
2025-02-18 16:16:36 +0900

    [Refactor] enum 변경

--- src/main/java/blaybus/domain/consulting/application/service/impl/CreateConsultingServiceImpl.java
+++ src/main/java/blaybus/domain/consulting/application/service/impl/CreateConsultingServiceImpl.java
@@ -64,7 +64,7 @@ public class CreateConsultingServiceImpl implements CreateConsultingService {
                 .user(user)
                 .designer(designer)
                 .meeting(findMeeting)
-                .type(ConsultingType.fromString(req.meet()))  // 적절한 타입으로 변경
+                .type(ConsultingType.valueOf(req.meet()))  // 적절한 타입으로 변경
                 .status(ConsultingStatus.fromString(status)) // 초기 상태 지정
                 .build();
 
